46c2e9b420ac25990ececeac9c576b733da7c855
brunofavs <bruno.favs@ua.pt>
2022-10-22 00:58:31 +0100

    Shebang line/ Code convenctions added

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#-----------
+# Conventions
+#-----------
+
+#! camelCase for functions and methods
+#! snake_case for variables
+
+#-----------
+# Imports
+#-----------
+
+
