1df600355078d2181638bf9c2403999051c20967
brunofavs <bruno.favs@ua.pt>
2022-10-30 15:45:52 +0000

    Advanced func1 fully functional

--- usp.py
+++ usp.py
@@ -10,20 +10,20 @@ import pick
 
 
 
-x,y = cc_centroids[max_label]
+# x,y = cc_centroids[max_label]
 
-centroid = centroid_tuple(x= int(x),y=int(y)) 
-
-Lx, Ly = cc_centroids['last_x'], cc_centroids['last_y']
+# centroid = centroid_tuple(x= int(x),y=int(y)) 
 
 
 
 
-def drawingLine(white_board,points,options):
+
+def drawingLine(white_board,points,options,usp):
 
     if points['x'][-1] == -50 :
         points['x'].pop(-1)
         points['y'].pop(-1)
+
 
     # If it pops, then it returns and doesnt draw anything, this prevents big lines across to (-50,-50)
 
@@ -33,16 +33,18 @@ def drawingLine(white_board,points,options):
     #It only reaches here if there are enough points to draw a line
 
     #* ---Drawing the line in the input image----
-    inicial_point = (points['x'][-2],points['y'][-2] )
+    inicial_point = (points['x'][-2],points['y'][-2] )  
     final_point = (points['x'][-1],points['y'][-1] )
 
-    if Lx !=-1 and Ly !=-1:
-        if usp: #chamar aqui a funçao do argparse
-            distance = sqrt((Lx-x)**2+(Ly-y)**2)
-            if distance > 100:
-                Lx = inicial_point
-                Ly = final_point
-        cv2.line(white_board, inicial_point, final_point, options['color'], options['size'])
+    #* ---Checking whether distance is good if ups----
+    if usp: 
+        distance = sqrt((inicial_point[0]-final_point[0])**2+(inicial_point[1]-final_point[1])**2)
+        if distance > 100:
+           return
+
+    #Python passes arguments by assignment, since a np array is mutable, we're just
+    #modifying the inicial image, not changing the original memory adress
+    cv2.line(white_board, inicial_point, final_point, options['color'], options['size'])
     
     return white_board
 
